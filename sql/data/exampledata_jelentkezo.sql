047b7445e511148f753eb3f3e924465b89db1a8b
MegaPet <jagerpeter04@gmail.com>
2025-03-27 23:31:38 +0100

    jelentkezo table + values, moderator table + values, allaskereso javitva

---
+++ sql/data/exampledata_jelentkezo.sql
@@ -0,0 +1,5 @@
+INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "", 0);
+INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "", 0);
+INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "", 0);
+INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "", 0);
+INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "", 0);
