33e533ba68235eb50c1b61110d8d81106eee444a
MegaPet <jagerpeter04@gmail.com>
2025-03-27 23:33:08 +0100

    jelentkezo value updatelve

--- sql/data/exampledata_jelentkezo.sql
+++ sql/data/exampledata_jelentkezo.sql
@@ -1,5 +1,5 @@
-INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "", 0);
-INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "", 0);
-INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "", 0);
-INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "", 0);
-INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "", 0);
+INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "john.doe@example.com", 1);
+INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "jane.smith@example.com", 2);
+INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "alice.wonderland@example.com", 3);
+INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "bob.builder@example.com", 4);
+INSERT INTO jelentkezo ( allaskereso_email, allaslehetoseg_id) VALUES ( "charlie.brown@example.com", 5);
