fcbf34466a26a0e4f8127c66d31e7104d242711b
Czuppon Pál Balázs <h256619@stud.u-szeged.hu>
2025-03-27 23:54:07 +0100

    FINISH

---
+++ sql/data/exampledata_moderator.sql
@@ -0,0 +1,5 @@
+INSERT INTO moderator VALUES ("examolemoderator1@moderator.com", "mod_1", "kiscica1");
+INSERT INTO moderator VALUES ("examolemoderator2@moderator.com", "mod_2", "kiscica2");
+INSERT INTO moderator VALUES ("examolemoderator3@moderator.com", "mod_3", "kiscica3");
+INSERT INTO moderator VALUES ("examolemoderator4@moderator.com", "mod_4", "kiscica4");
+INSERT INTO moderator VALUES ("examolemoderator5@moderator.com", "mod_5", "kiscica5");
