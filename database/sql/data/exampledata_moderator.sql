a4271cef261bd507c2ceccd8a9595b109ca6c4c6
Czuppon Pál Balázs <h256619@stud.u-szeged.hu>
2025-05-14 23:32:25 +0200

    Revert "example data in data folder and in database_all"
    This reverts commit 97625f246363d4c5ce8cfe13f00a0a97d3bdb208.

--- database/sql/data/exampledata_moderator.sql
+++ database/sql/data/exampledata_moderator.sql
@@ -1,33 +1,5 @@
-Insert into moderator (EMAIL,NEVE,JELSZO) values
-('admin@gmail.com','admin','b6b46253f3a26b7f23ec573cb679d6afaeaf0dc3ada39903b93204edac097ae4'),
--- ('admin@gmail.com', 'admin', 'admin');
-('czuppon.pal@gmail.com','Czuppon Pál Balázs','7d801da9da1ed086f197f7309a9c3c213ccc214c673b2f4a130935780ad71acd'),
--- ('czuppon.pal@gmail.com','Czuppon Pál Balázs', 'admin')
-('jagerpeter04@gmail.com','Jáger Péter','4cb25b69c77aabf8f74461beb8f3bdd8a644aba4364731631cf1aada31fceed7'),
--- ('jagerpeter04@gmail.com','Jáger Péter', 'admin'),
-('mod2@allas.hu','Szabó Katalin','eb5fae9a4c9ccc7ee2324625a003ea351e1f2823414e01d6cf00604167d7998a'),
--- ('mod2@allas.hu', 'Szabó Katalin', 'mod456'),
-('mod3@allas.hu','Nagy István','a521d89cc408d48b7a0cb1402ac9376b1c6cf8e81bd8099d63fea3843517f199'),
--- ('mod3@allas.hu', 'Nagy István', 'mod789'),
-('mod4@allas.hu','Tóth Eszter','26b02ba94c72a13f1fb7ce05f01e483e59d1dd54da77f97e2578d67747320dbe'),
--- ('mod4@allas.hu', 'Tóth Eszter', 'mod101'),
-('mod5@allas.hu','Horváth Gábor','7528597a7f4a0c106e320629e847b7f9f5d58c5587f184867202f40cc7b68805'),
--- ('mod5@allas.hu', 'Horváth Gábor', 'mod102'),
-('mod6@allas.hu','Kiss Anna','94098bcc8477c5d9d5c2654c5cd2db5cb3757fb089ac6f02836a10f57a2aab3b'),
--- ('mod6@allas.hu', 'Kiss Anna', 'mod103'),
-('mod7@allas.hu','Molnár Péter','1985a87fa0239758e9cfb2d8e8acc412158e5b9cb4350d1add1ea63c0b5f0703'),
--- ('mod7@allas.hu', 'Molnár Péter', 'mod104'),
-('mod8@allas.hu','Farkas Zsófia','bda0cb75f8c9a42627b626a95713d088d3f297121fb83a60c4360ddef2f867e3'),
--- ('mod8@allas.hu', 'Farkas Zsófia', 'mod105'),
-('mod9@allas.hu','Balogh László','b004fec26781cecb1535581053585d8670743973a757e390aa7b12cd0d9d55b2'),
--- ('mod9@allas.hu', 'Balogh László', 'mod106'),
-('mod10@allas.hu','Papp Viktória','fd656d87105befa552c241028657f9f17a39c36f6afffa1b6d5cfd081ab7dc9f'),
--- ('mod10@allas.hu', 'Papp Viktória', 'mod107'),
-('mod11@allas.hu','Simon Róbert','3be225ec0a07b5135de492311500a08137278980c3defa6505b7f567ca4b9857'),
--- ('mod11@allas.hu', 'Simon Róbert', 'mod108'), --
-('mod12@allas.hu','Veres Júlia','476757286c7edd30f980e938d2552cf9e4dced3b67e6a1d6a3f2842b89852371'),
--- ('mod12@allas.hu', 'Veres Júlia', 'mod109'),
-('mod13@allas.hu','Lukács Tamás','502d1c5a4994402dbd356993283489a313175dc061ff3e025b1738fe5896dc12'),
--- ('mod13@allas.hu', 'Lukács Tamás', 'mod110'),
-('mod14@allas.hu','Orosz Dóra','afdf0761206e1eebf916dfc6564a289545f2c71f56214550aafce635d963a18b');
--- ('mod14@allas.hu', 'Orosz Dóra', null);
+INSERT INTO moderator VALUES ("examolemoderator1@moderator.com", "mod_1", "kiscica1");
+INSERT INTO moderator VALUES ("examolemoderator2@moderator.com", "mod_2", "kiscica2");
+INSERT INTO moderator VALUES ("examolemoderator3@moderator.com", "mod_3", "kiscica3");
+INSERT INTO moderator VALUES ("examolemoderator4@moderator.com", "mod_4", "kiscica4");
+INSERT INTO moderator VALUES ("examolemoderator5@moderator.com", "mod_5", "kiscica5");
